daffc4f1403b92b0fe31a65ad8733ef3cd05e63d
baconsenpaii <baconforevery2@gmail.com>
2022-11-17 16:50:30 +0000

    change greeting to add `neofetch`

--- fish/fish_greeting.fish
+++ fish/fish_greeting.fish
@@ -3,4 +3,5 @@ function fish_greeting
 	echo \n(set_color bryellow)Welcome!\n(set_color normal)
 	echo (set_color white)Running WSL with (set_color brmagenta)"$WSL_DISTRO_NAME" (cat /etc/debian_version)(set_color normal)
 	echo (set_color white)Starting (set_color brmagenta)(basename "$SHELL") "$FISH_VERSION"(set_color white) in (set_color brcyan)"$PWD"\n
+	neofetch
 end
